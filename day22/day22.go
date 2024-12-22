de94b4390d9a30b0c4b65f6f501163d4f889215f
Andrew Fletcher <andrew.fletcher@kps.com>
2024-12-22 23:32:07 +0000

    Marginally quicker and less indented but still wrong

--- day22/day22.go
+++ day22/day22.go
@@ -50,7 +50,7 @@ func main() {
 	for _, n := range numbers {
 		changes := [][2]int{}
 		for i := 0; i <= 2000; i++ {
-			ones := n - (n / 10 * 10)
+			ones := n % 10
 			if i > 0 {
 				diff := ones - prev
 				changes = append(changes, [2]int{ones, diff})
@@ -64,38 +64,39 @@ func main() {
 		allbuyers = append(allbuyers, changes)
 	}
 
-	max := 0
-	for s1 := -9; s1 < 10; s1++ {
-		for s2 := -9; s2 < 10; s2++ {
-			for s3 := -9; s3 < 10; s3++ {
-				for s4 := -9; s4 < 10; s4++ {
-					sequence := [4]int{s1, s2, s3, s4}
-					prices := make([]int, len(allbuyers))
+	allSeqs := make(map[[4]int]bool)
+	for _, buyer := range allbuyers {
+		for i := 3; i < len(buyer); i++ {
+			seq := [4]int{buyer[i-3][1], buyer[i-2][1], buyer[i-1][1], buyer[i][1]}
+			allSeqs[seq] = true
+		}
+	}
 
-				BUYER:
-					for b, buyer := range allbuyers {
-						i := 0
-						for _, change := range buyer {
-							if change[1] == sequence[i] {
-								if i == 3 {
-									prices[b] = change[0]
-									continue BUYER
-								}
-								i += 1
-							} else {
-								i = 0
-							}
-						}
+	max := 0
+	for seq := range allSeqs {
+		prices := make([]int, len(allbuyers))
+
+	BUYER:
+		for b, buyer := range allbuyers {
+			i := 0
+			for _, change := range buyer {
+				if change[1] == seq[i] {
+					if i == 3 {
+						prices[b] = change[0]
+						continue BUYER
 					}
-					total = 0
-					for _, p := range prices {
-						total += p
-					}
-					if total > max {
-						max = total
-					}
+					i += 1
+				} else {
+					i = 0
 				}
 			}
+		}
+		total = 0
+		for _, p := range prices {
+			total += p
+		}
+		if total > max {
+			max = total
 		}
 	}
 	fmt.Println("Part 2:", max)
