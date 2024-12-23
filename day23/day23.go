30920fe9ddefe46c46067401ab77a81c1a4627c6
Andrew Fletcher <andrew.fletcher@kps.com>
2024-12-23 14:27:15 +0000

    Added day23 pt 2 + README

--- day23/day23.go
+++ day23/day23.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 )
 
+var connMap = make(map[string][]string)
+
 func main() {
 	flag.Parse()
 	input := "input.txt"
@@ -23,7 +25,6 @@ func main() {
 	}
 	defer f.Close()
 
-	connMap := make(map[string][]string)
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		parts := strings.Split(scanner.Text(), "-")
@@ -33,32 +34,19 @@ func main() {
 		connMap[to] = append(connMap[to], from)
 	}
 
+	// pt1
 	conns := [][]string{}
 	for from := range connMap {
-		conns = append(conns, connections(connMap, from, []string{})...)
+		conns = append(conns, connections(from, []string{})...)
 	}
-	// slices.SortFunc(conns, func(a []string, b []string) int {
-	// 	for i := 0; i < 3; i++ {
-	// 		c := strings.Compare(a[i], b[i])
-	// 		if c == 0 {
-	// 			continue
-	// 		}
-	// 		return c
-	// 	}
-	// 	return 0
-	// })
-
 	deduped := [][]string{}
 	for _, c := range conns {
 		if !slices.ContainsFunc(deduped, func(c1 []string) bool {
-			slices.Sort(c)
-			slices.Sort(c1)
 			return slices.Compare(c, c1) == 0
 		}) {
 			deduped = append(deduped, c)
 		}
 	}
-
 	total := 0
 	for _, c := range deduped {
 		for i := 0; i < 3; i++ {
@@ -69,11 +57,34 @@ func main() {
 		}
 	}
 
-	fmt.Println(total)
+	// pt 2
+	allCliques := [][]string{}
+	for from := range connMap {
+		cliques := createCliques(from)
+		for _, c := range cliques {
+			if !slices.ContainsFunc(allCliques, func(c1 []string) bool {
+				return slices.Compare(c, c1) == 0
+			}) {
+				allCliques = append(allCliques, c)
+			}
+		}
+	}
+
+	maxlen := 0
+	var maxlan []string
+	for _, c := range allCliques {
+		if len(c) > maxlen {
+			maxlen = len(c)
+			maxlan = c
+		}
+	}
+
+	fmt.Println("Part 1:", total)
+	fmt.Println("Part 2:", strings.Join(maxlan, ","))
 
 }
 
-func connections(connMap map[string][]string, from string, path []string) [][]string {
+func connections(from string, path []string) [][]string {
 	if len(path) > 3 {
 		return [][]string{}
 	}
@@ -81,11 +92,38 @@ func connections(connMap map[string][]string, from string, path []string) [][]st
 	res := [][]string{}
 	for _, neighbour := range connMap[from] {
 		if len(path) == 3 && path[0] == neighbour {
+			slices.Sort(path)
 			return [][]string{path}
 		}
 		if !slices.Contains(path, neighbour) {
-			res = append(res, connections(connMap, neighbour, path)...)
+			res = append(res, connections(neighbour, path)...)
 		}
 	}
 	return res
+}
+
+func createCliques(from string) (cliques [][]string) {
+	for _, neighbour := range connMap[from] {
+		clique := []string{from, neighbour}
+		for node, neighbours := range connMap {
+			if node == from {
+				continue
+			}
+			if containsAll(neighbours, clique) {
+				clique = append(clique, node)
+			}
+		}
+		slices.Sort(clique)
+		cliques = append(cliques, clique)
+	}
+	return
+}
+
+func containsAll(haystack []string, needles []string) (containsAll bool) {
+	for _, needle := range needles {
+		if !slices.Contains(haystack, needle) {
+			return false
+		}
+	}
+	return true
 }
